412e7430a2842ac71fc8a616b99065e5eb320899
yogyadav137 <yogyadav@cisco.com>
2016-08-08 21:24:06 -0400

    Add files via upload

---
+++ InfraHealthBot.py
@@ -0,0 +1,71 @@
+from itty import *
+import urllib2
+import json
+import shlex
+import os
+from subprocess import *
+import helloWorld
+from disk_usage import sparkpart, sparktot, sparkused, bytes2human, get_disk_value, main
+from meminfo import pprint_ntuple, get_mem_info
+import psutil
+
+#sparktot = 0
+
+def sendSparkGET(url):
+    """
+    This method is used for:
+        -retrieving message text, when the webhook is triggered with a message
+        -Getting the username of the person who posted the message if a command is recognized
+    """
+    request = urllib2.Request(url,
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents
+    
+def sendSparkPOST(url, data):
+    """
+    This method is used for:
+        -posting a message to the Spark room to confirm that a command was received and processed
+    """
+    request = urllib2.Request(url, json.dumps(data),
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents
+
+@post('/')
+def index(request):
+    """
+    When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
+    using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
+    further actions are taken. i.e.
+    /batman    - replies to the room with text
+    /batcave   - echoes the incoming text to the room
+    /batsignal - replies to the room with an image
+    """
+    webhook = json.loads(request.body)
+    print webhook['data']['id']
+    result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
+    result = json.loads(result)
+    msg = None
+    if webhook['data']['personEmail'] != bot_email:
+        in_message = result.get('text', '').lower()
+        in_message = in_message.replace(bot_name, '')
+        if 'disk_usage' in in_message:
+                res = (get_disk_value())
+                print(res)
+                msg = format(res)
+        if msg != None:
+            print msg
+            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
+    return "true"
+
+####CHANGE THESE VALUES#####
+bot_email = "infrahealthbot@sparkbot.io"
+bot_name = "infrahealthbot"
+bearer = "ZWFmZjJmNTItM2FlNS00NWVkLTg5ZTEtNjVhNWYwYzlhN2JlMGExYjBhNDUtZTFh"
+bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
+run_itty(server='wsgiref', host='0.0.0.0', port=10010)
